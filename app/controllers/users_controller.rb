728e0b7dacf15f8332da64208a75f359635bc2e9
Amonsta <amana.ammi@gmail.com>
2021-02-18 22:02:15 -0700

    create method and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if user.save
+            redirect_to user_path(@user) # code this
+        else
+            render :new
+        end
+    end
+
 private 
 
     def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :photo)
     end
 end
